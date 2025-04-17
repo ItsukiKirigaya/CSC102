a796c6eec38546aa6c9e749a6be4a85a09c8e43a
ItsukiKirigaya <144621133+ItsukiKirigaya@users.noreply.github.com>
2025-04-17 11:25:59 -0500

    Add files via upload

---
+++ Game File/code.js
@@ -0,0 +1,7 @@
+function updateText() {
+    // Defines a function named 'updateText'. This function can be called when an event (like a button click) occurs.
+
+    document.getElementById("heading").innerHTML = "Games are Awesome";
+    // Finds the HTML element with the ID 'heading' (your <h1> tag)
+    // and changes its inner HTML content to display the text "Games are Awesome".
+}
